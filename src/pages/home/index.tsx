a21d028796d2e789ccf47901d52727728f943e03
Andres <andrestone9191@gmail.com>
2023-07-15 14:17:47 -0300

    0.1.1

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { TextTitle } from "ui/Texts";
 import { Text } from "ui/Texts";
-import { ButtonLocation } from "ui/buttons";
+import { MainButton } from "ui/buttons";
 import css from "./index.css";
+import { useLocation } from "hooks";
 
 function Home() {
+  const [location, setLocation] = useLocation();
+
+  const handlerClick = async () => {
+    navigator.geolocation.getCurrentPosition((e) => {
+      const lng = e.coords.longitude as any;
+      const lat = e.coords.latitude as any;
+      setLocation({ lng, lat });
+    });
+  };
+
+  useEffect(() => {
+    if (location) {
+      console.log(location);
+    }
+  }, [location]);
+
   return (
     <div className={css.contenedor}>
       <TextTitle children="Mascotas perdidas cerca tuyo"></TextTitle>
@@ -12,7 +29,8 @@ function Home() {
         Para ver las mascotas reportadas cerca tuyo necesitamos permiso para
         conocer tu ubicación.
       </Text>
-      <ButtonLocation children="Dar mi Ubicacion"></ButtonLocation>
+      <MainButton click={handlerClick} children="Dar mi Ubicacion"></MainButton>
+      <div>{location.lat}</div>
     </div>
   );
 }
--- src/ui/buttons/index.tsx
+++ src/ui/buttons/index.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import css from "./index.css";
 
-function handlerbutton() {
-  navigator.geolocation.getCurrentPosition((e) => {
-    const lng = e.coords.longitude as any;
-    const lat = e.coords.latitude as any;
-    console.log(lng, lat);
-  });
-}
-function ButtonLocation(props) {
+export function MainButton(props) {
   return (
-    <button onClick={handlerbutton} className={css.root}>
+    <button onClick={props.click} className={css.root}>
       {props.children}
     </button>
   );
 }
-export { ButtonLocation };
